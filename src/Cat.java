0171d630512bd072bd2170778ea927bdcd574145
Megan Hibler <mhibler@mines.edu>
2022-09-29 12:56:12 -0600

    I changed the color of the cat to orange, the mouth color to red, and changed the dimension of the mouth and the X position of the mouth as well

--- src/Cat.java
+++ src/Cat.java
@@ -16,9 +16,9 @@ public class Cat {
 	private static final int EYE_WIDTH = 10;
 	// pick mouth height, width is based on head dimension
 	private static final int MOUTH_HEIGHT = 10;
-	private static final int MOUTH_WIDTH = HEAD_DIMENSION/4;
+	private static final int MOUTH_WIDTH = HEAD_DIMENSION/2;
 	// mouth starts about 40% from left edge of head
-	private static final int MOUTH_X = HEAD_DIMENSION/5 * 2;
+	private static final int MOUTH_X = HEAD_DIMENSION/5 * 1;
 	// changed mouth_x to better show cat with new dimensions set for eye.
 	private static final int MOUTH_Y = HEAD_DIMENSION/5 * 3;
 	
@@ -35,7 +35,7 @@ public class Cat {
 		int x=catX;
 		int y=catY;
 		// Draw the head
-		g2.setColor(Color.gray);
+		g2.setColor(Color.ORANGE);
 		//changed head color to gray
 		g2.fillOval(x, y, HEAD_DIMENSION, HEAD_DIMENSION);
 		// Draw the eyes
@@ -47,7 +47,7 @@ public class Cat {
 		x += EYE_SEPARATION;
 		g2.fillOval(x, y, EYE_WIDTH, EYE_HEIGHT);
 		// Draw the mouth
-		g2.setColor(Color.pink);
+		g2.setColor(Color.red);
 		x = catX + MOUTH_X;
 		y = catY + MOUTH_Y;
 		g2.fillOval(x, y, MOUTH_WIDTH, MOUTH_HEIGHT);
